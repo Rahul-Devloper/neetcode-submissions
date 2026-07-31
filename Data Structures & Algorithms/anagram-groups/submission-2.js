ef55add1a10752942a2b04867db617cc95da5c02
Rahul <rahulramesh26@yahoo.com>
2026-07-31 13:17:14 +0100

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        let map = new Map();
+        let output
+        for (let i = 0; i < strs.length; i++) {
+            output = checkAnagram(strs[i], i);
+        }
+            return output
+
+
+        function checkAnagram(str, i) {
+            let key = str.split("").sort().join("");
+            if (!map.has(key)) {
+                map.set(key, [str]);
+            } else {
+                let value = map.get(key);
+                map.set(key, [...value, str]);
+            }
+            return Array.from(map.values())
+
+        }
+    }
+}
